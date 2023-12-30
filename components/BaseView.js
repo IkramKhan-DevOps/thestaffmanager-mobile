e8b59bc26cab2d40011521ee337830ac57ccd2a8
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2023-12-31 00:40:21 +0500

    DEL: files are deleted

--- components/BaseView.js
+++
@@ -1,9 +0,0 @@
-import {StatusBar, View} from "react-native";
-
-export default function BaseView({children}) {
-    return (
-        <View style={{marginTop:StatusBar.currentHeight||0, flex:1}}>
-            { children }
-        </View>
-    )
-}
